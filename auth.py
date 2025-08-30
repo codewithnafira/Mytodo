2599d3602532bad58bfe820fab936f82b9e73c6f
Mulunesh Geleto <derarobed@gmail.com>
2025-08-30 19:53:41 +0300

    Initial commit

---
+++ auth.py
@@ -0,0 +1,44 @@
+from flask import render_template, request, redirect, flash, url_for
+from app import app, db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email already registered!", "danger")
+            return redirect(url_for('register'))
+
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registration successful! Please log in.", "success")
+        return redirect(url_for('todo_home'))
+
+    return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid credentials!", "danger")
+
+    return render_template("login.html")
+
---
+++ todo.py
@@ -0,0 +1,60 @@
+from flask import render_template, request, redirect, flash, url_for
+from app import app, db
+from datetime import datetime
+
+# Todo model
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<{self.sno} --- {self.title}>'
+
+# Todo routes
+@app.route('/', methods=['GET', 'POST'])
+def todo_home():
+    if request.method == 'POST':
+        todotitle = request.form.get('todotitle')
+        tododesc = request.form.get('tododesc')
+
+        if todotitle:  # prevent empty title
+            new_todo = Todo(title=todotitle, desc=tododesc)
+            db.session.add(new_todo)
+            db.session.commit()
+            flash("Todo added successfully!", "success")
+            return redirect(url_for('todo_home'))
+
+        flash("Title cannot be empty!", "danger")
+
+    todos = Todo.query.all()
+    return render_template("home.html", todos=todos)
+
+
+@app.route('/delete/<int:sno>')
+def delete_todo(sno):
+    todo = Todo.query.get_or_404(sno)
+    db.session.delete(todo)
+    db.session.commit()
+    flash("Todo deleted!", "info")
+    return redirect(url_for('todo_home'))
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update_todo(sno):
+    todo = Todo.query.get_or_404(sno)
+
+    if request.method == 'POST':
+        new_title = request.form.get('todotitle')
+        new_desc = request.form.get('tododesc')
+
+        if new_title:
+            todo.title = new_title
+            todo.desc = new_desc
+            db.session.commit()
+            flash("Todo updated successfully!", "success")
+            return redirect(url_for('todo_home'))
+        else:
+            flash("Title cannot be empty!", "danger")
+
+    # GET request → show update form
+    return render_template("update.html", todo=todo)
